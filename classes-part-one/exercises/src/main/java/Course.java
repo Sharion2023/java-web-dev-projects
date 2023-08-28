615fbc68aa20d2087aecdadf335b1aff717e526d
Sharion2023 <Sharion06@gmail.com>
2023-08-28 11:48:24 -0500

    Finished.

---
+++ classes-part-one/exercises/src/main/java/Course.java
@@ -0,0 +1,7 @@
+import java.util.ArrayList;
+
+public class Course {
+    private String instructor;
+    private String topic;
+    private ArrayList <Student> enrolledStudents;
+}
--- classes-part-one/exercises/src/main/java/Student.java
+++ classes-part-one/exercises/src/main/java/Student.java
@@ -5,6 +5,31 @@ public class Student {
         private double gpa = 0.0;
 
         // Drop your getters and setters below for the Student class.
+        public String getName() {
+                return name;
+        }
+        public int getStudentId(){
+                return studentId;
+        }
+        private int getNumberOfCredits(){
+                return numberOfCredits;
+        }
+        public double getGpa(){
+                return gpa;
+        }
+
+        public void setName(String name){
+               this.name = name;
+        }
+        public void setStudentId(int studentId){
+                this.gpa = gpa;
+        }
+        public void setNumberOfCredits(int numberOfCredits){
+                this.numberOfCredits = numberOfCredits;
+        }
+        public void setGpa(double gpa){
+                this.gpa = gpa;
+        }
         // To instantiate the Student class, add your code to the main in the file, SchoolPractice.
 
 }
